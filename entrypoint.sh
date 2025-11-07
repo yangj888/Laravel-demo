82ea84f295b6cf46ff760532afc5bbd7730cceb9
yangj888 <31609794+yangj888@users.noreply.github.com>
2025-11-07 18:04:50 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Start Nginx service
+service nginx start
+# Run Laravel migrations
+php artisan migrate --force
+# Create symbolic link for storage
+php artisan storage:link
+# Clear and optimize the application cache
+php artisan optimize:clear
+php artisan optimize
+# Start PHP-FPM
+php-fpm
